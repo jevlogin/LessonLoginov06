cf377f5e07e3ec96963079d6c958ebd4cf00dcdd
jevlogin <jev.login@mail.ru>
2020-07-17 16:10:30 +0300

    Создал библиотеку с клаасом Pause()

---
+++ Pause/PauseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace MyLib
+{
+    static public class PauseClass
+    {
+        static public void Pause()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ les02/Program.cs
@@ -0,0 +1,34 @@
+﻿using static MyLib.PauseClass;
+using System;
+using System.IO;
+
+
+namespace les02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream fileIn = new FileStream("data.txt", FileMode.Open, FileAccess.Read);
+                FileStream fileOut = new FileStream("newData.txt", FileMode.Create, FileAccess.Write);
+                int i;
+                while((i = fileIn.ReadByte()) != -1)
+                {
+                    //  запись очередного байта в поток, связанный с файлом fileOut
+                    fileOut.WriteByte((byte)i);
+                }
+                fileIn.Close();
+                fileOut.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Файл успешно скопирован");
+
+            Pause();
+        }
+    }
+}
