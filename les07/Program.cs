0c9981f3b324b94ce3a80e160c7fe6fe0b1e60d3
jevlogin <jev.login@mail.ru>
2020-07-17 21:30:31 +0300

    необобщённые списки - ArrayList

---
+++ les07/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using static MyLib.PauseClass;
+
+namespace les07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfBachelors = 0;
+            int numOfMasters = 0;
+            // Создадим необобщенный список
+            ArrayList list = new ArrayList();
+            // Запомним время в начале обработки данных
+            DateTime dt = DateTime.Now;
+            //StreamReader sr = new StreamReader("data.csv");
+            StreamReader sr = new StreamReader("..\\..\\students.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(',');
+
+                    list.Add(s[1] + " " + s[0]);    // Добавляем склееные имя и фамилию
+                    if (int.Parse(s[6]) < 5)
+                    {
+                        numOfBachelors++;
+                    }
+                    else
+                    {
+                        numOfMasters++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            sr.Close();
+            list.Sort();
+
+            Console.WriteLine($"Всего студентов: {list.Count}");
+            Console.WriteLine($"Магистров: {numOfMasters}");
+            Console.WriteLine($"Бакалавров: {numOfBachelors}");
+            Console.WriteLine("\n");
+
+
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine(DateTime.Now - dt);
+
+            Pause();
+        }
+    }
+}
