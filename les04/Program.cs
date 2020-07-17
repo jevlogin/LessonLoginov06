8ff32aed4218d8b6dea16a7647710b448bf13306
jevlogin <jev.login@mail.ru>
2020-07-17 16:56:58 +0300

    Двоичный поток data.bin

---
+++ les04/Program.cs
@@ -0,0 +1,40 @@
+﻿using static MyLib.PauseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace les04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                FileStream f = new FileStream("data.bin", FileMode.Open);
+                BinaryReader fIn = new BinaryReader(f);
+                long n = f.Length / 4;
+
+                int a;
+                for (a = 0; a < n; a++)
+                {
+                    a = fIn.ReadInt32();
+                    Console.WriteLine(a + " ");
+                }
+
+                fIn.Close();
+                f.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Pause();
+        }
+    }
+}
