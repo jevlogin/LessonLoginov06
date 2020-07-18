2af53dd93d530cea315af0f93007d245fb8b0733
jevlogin <jev.login@mail.ru>
2020-07-18 12:27:29 +0300

    отсортировать массив целых чисел каким-то своеобразным способом

---
+++ les12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using static MyLib.PauseClass;
+
+
+namespace les12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Получаем список файлов в папке. AllDirectories - сканировать также и подпапки
+            try
+            {
+                string[] fs = Directory.GetFiles("D:\\Temp", "*.*", SearchOption.AllDirectories);
+                foreach (var fileName in fs)
+                {
+                    // Создаем регулярное выражения дя поиска почтовых адресов
+                    Regex regex = new Regex(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})");
+                    // Считываем файл
+                    string s = File.ReadAllText(fileName);
+                    Console.WriteLine(fileName);
+                    // Выводим найденные адреса на экран
+                    foreach (var c in regex.Matches(s))
+                    {
+                        Console.WriteLine($"{c}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            // Просматриваем каждый файл в массиве
+            
+
+            Pause();
+        }
+    }
+}
---
+++ les13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static MyLib.PauseClass;
+
+namespace les13
+{
+    class Program
+    {
+        static int Compare(int a, int b)
+        {
+            if (a % 10 > b % 10)
+            {
+                return 1;
+            }
+            if (a % 10 < b % 10)
+            {
+                return -1;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = new int[20];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = i;
+            }
+            Array.Sort(a, Compare);
+            foreach (var e in a)
+            {
+                Console.WriteLine($"{e,4}");
+            }
+
+            Pause();
+        }
+    }
+}
