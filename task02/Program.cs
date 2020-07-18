69ab3cbb4af1195b625bff1d9fcef0f0e2ace921
jevlogin <jev.login@mail.ru>
2020-07-18 15:28:03 +0300

    делегаты зло

--- task02/Program.cs
+++ task02/Program.cs
@@ -23,56 +23,97 @@ using static MyLib.PauseClass;
 namespace task02
 {
     public delegate double Fun(double x);
+    public delegate double Fun2(string fileName, double a, double b, double h);
+
+    public delegate void ObsDelegate();
+
+    public delegate void SaveDelegate(string fileName, double a, double b, double h);
+    public delegate void LoadDelegate(string fileName);
+
     class Program
     {
         public static double F(double x)
         {
             return (x * x) - (50 * x) + 10;
         }
-
-        public static void SaveFunc(string fileName, Fun DF, double a, double b, double h)
+        public static void Save(string fileName, double a, double b, double h)
         {
             FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
             BinaryWriter bw = new BinaryWriter(fs);
+
             double x = a;
             while (x <= b)
             {
-                bw.Write(DF(x));
+                bw.Write(F(x));
                 x += h;
             }
             bw.Close();
             fs.Close();
         }
+        public static void SaveFunc(string fileName, Fun DF, double a, double b, double h)
+        {
+            try
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                BinaryWriter bw = new BinaryWriter(fs);
+
+                double x = a;
+                while (x <= b)
+                {
+                    bw.Write(DF(x));
+                    x += h;
+                }
+                bw.Close();
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
 
         public static double Load(string fileName)
         {
-            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            BinaryReader br = new BinaryReader(fs);
-
-            double min = double.MaxValue;
-            double d;
-
-            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            try
             {
-                d = br.ReadDouble();
-                if (d < min)
-                {
-                    min = d;
-                }
-            }
-            br.Close();
-            fs.Close();
+                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
 
-            return min;
+                double min = double.MaxValue;
+                double d;
+
+                for (int i = 0; i < fs.Length / sizeof(double); i++)
+                {
+                    d = br.ReadDouble();
+                    if (d < min)
+                    {
+                        min = d;
+                    }
+                }
+                br.Close();
+                fs.Close();
+                return min;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
         }
         static void Main(string[] args)
         {
             const string fileName = @"data.bin";
 
-            SaveFunc(fileName, F, -100, 100, 0.5);
+            //  SaveFunc(fileName, F, -100, 100, 0.5);
+            //Save(fileName, -100, 100, 0.5);
+
+
+
+
             System.Console.WriteLine(Load(fileName));
 
             Pause();
         }
+
     }
 }
